28e72c805557df5521a5bbcf805133b5d934b129
dsridhar91 <dsridhar@ucsc.edu>
2014-05-21 14:40:25 -0700

    5/21 trying to fix pathway pairs data

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -8,6 +8,7 @@ import scipy
 from numpy import argmax
 import itertools
 import csv
+import sys
 import numexpr
 
 score_types = ['protein1', 'protein2', 'neighborhood', 'fusion',  'cooccurence',  'coexpression', 'experimental', 'database', 'textmining']
@@ -16,9 +17,9 @@ output_directory_name = '/Users/dhanyasridhar/Documents/psl-pathways/edu.ucsc.cs
 
 
 def main():
-    create_string_similarity_scores()
-    create_ground_truth()
-
+    # create_string_similarity_scores()
+    # create_ground_truth()
+    create_drug_sensitivity_similarity()
 
 def create_string_similarity_scores():
     string_db = 'string-db.txt'
@@ -31,6 +32,7 @@ def create_string_similarity_scores():
 
     uniqueness = scores['protein1'] != scores['protein2']
     gene_pairs = scores[uniqueness][['protein1', 'protein2']]
+    gene_pairs = gene_pairs.drop_duplicates(['protein1', 'protein2'])
     gene_pairs.to_csv(output_directory_name + 'pathwayPairs.txt', sep='\t', header=False, index=False)
 
     for score in (set(score_types) - set(['protein1', 'protein2'])):
@@ -54,6 +56,22 @@ def create_ground_truth():
         for i in xrange(1, len(pathway)):
             for j in xrange(i + 1, len(pathway)):
                 known_pathway_pairs.writerow([pathway[i], pathway[j]])
+
+
+def create_drug_sensitivity_similarity():
+    matrix = pandas.read_csv('/Users/dhanyasridhar/Documents/yeast_data/HOP_scores.txt', sep="\t", index_col=0)
+    #remove any rows where there are nans
+    sub_matrix=matrix[~numpy.isnan(matrix).any(axis=1)]
+    #get the correlation coefficients of rows
+    co=numpy.corrcoef(sub_matrix)
+    nz=numpy.nonzero(co>0)
+
+    drug_similarity = csv.writer(open(output_directory_name + 'drugSensitivity.txt', 'wb'), delimiter='\t', quoting=csv.QUOTE_NONE)
+
+    for x, y in zip(nz[0], nz[1]):
+        if x != y:
+            drug_similarity.writerow([sub_matrix.axes[0][x], sub_matrix.axes[0][y], co[x][y]])
+            # print sub_matrix.axes[0][x], sub_matrix.axes[0][y], co[x][y]
 
 
 main()
